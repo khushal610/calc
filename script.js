079c8a957b77fefeacf9edaf7d7b306f0bd9ba1d
khushalprogrammer <112861132+khushalprogrammer@users.noreply.github.com>
2023-07-22 08:12:17 +0530

    added files

---
+++ script.js
@@ -0,0 +1,19 @@
+let string = " ";
+let buttons = document.querySelectorAll(".buttons");
+Array.from(buttons).forEach((buttons) => {
+    buttons.addEventListener("click", (e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector("input").value = string;
+        }
+        else if(e.target == 'c'){
+            string = " ";
+            document.querySelector("input").value = string;
+        }
+        else{ 
+            // console.log(e.target);
+            string = string + e.target.innerHTML;
+            document.querySelector("input").value = string;
+        }
+    });
+});
